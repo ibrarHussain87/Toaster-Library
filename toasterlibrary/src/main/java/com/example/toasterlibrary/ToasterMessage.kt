4079bae8da1eca78cea303bf3b9684d3877d9b24
Ibrar <ibrar@Ibrars-MacBook-Pro.local>
2022-08-24 11:30:26 +0500

    change code as companine objet

--- toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
@@ -7,7 +7,9 @@ import android.widget.Toast
 Created by ibrar on 24/08/2022
  */
 class ToasterMessage {
-    fun showToastMessage(context: Context,message:String){
-        Toast.makeText(context,message,Toast.LENGTH_LONG).show()
+    companion object {
+        fun showToastMessage(context: Context, message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
     }
 }
